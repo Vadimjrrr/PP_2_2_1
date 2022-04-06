62ad502b410a67808c029e1b138054c50a0a04a0
Вадим <Vadyok95@mail.ru>
2022-04-06 10:20:36 +0500

    Initial commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -57,6 +57,28 @@ public class Car {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (series != car.series) return false;
+        if (id != null ? !id.equals(car.id) : car.id != null) return false;
+        if (model != null ? !model.equals(car.model) : car.model != null) return false;
+        return user != null ? user.equals(car.user) : car.user == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        result = 31 * result + series;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "Car{" +
                 "id=" + id +
